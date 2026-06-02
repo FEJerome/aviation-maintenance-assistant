cdfd5830f37e0d442fbac6105db43f7571d9c985
fe-jerome <1159347298@qq.com>
2026-06-02 11:27:00 +0800

    first commit

---
+++ src/main/java/cn/pandazi/aviation_maintenance_assistant/AviationMaintenanceAssistantApplication.java
@@ -0,0 +1,13 @@
+package cn.pandazi.aviation_maintenance_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationMaintenanceAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviationMaintenanceAssistantApplication.class, args);
+	}
+
+}
